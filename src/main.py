33bcc963039d89abd81d0abcb2485ed10c9d3c87
Jaraare <hoomanempty@gmail.com>
2026-08-14 15:39:52 +0330

    feat : Show Password. - Add show_password function to show the password that the user has saved. - Ask question for create new password or show previous passwords (create new password dosen't remove previous passwords). - Show where passwords are saved.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from random import randint as ri
 from input_handler import input_handler
 import encrypt
+from pathlib import Path
+import json
 
 letters = "abcdefghijklmnopqrstuvwxyz"
 symbols = "!@#$%^&*()_+-={[}]\\/:;|,.<>?'"
@@ -37,12 +39,12 @@ def mixer(include_symbols, include_numbers):
 
     return mixed
 
-def saving(what_save, password):
+def save_password(want_save, password):
     save = []
     if want_save == password:
         save = password
 
-    elif want_save == "0":
+    elif want_save == 0:
         return 0
     
     elif want_save:
@@ -59,29 +61,59 @@ def saving(what_save, password):
                 save.append(password[int(number_saver) - 1])
                 number_saver = ""
 
-    with open("output.txt", "a") as file:
-        for i in save:
-            file.write(f"{encrypt.encoding(i)}")
+    password_dict = {"encrypted_password" : [],
+                     "key" : []
+                    } 
+    
+    for item in save:
+        key, encrypted_password = encrypt.encoding(item)
+        password_dict["encrypted_password"].append(encrypted_password.decode())
+        password_dict["key"].append(key.decode())
+        
+    with open("output.Jaraare", "a") as file:
+        file.write(json.dumps(password_dict))
+
+def show_password(path):
+    passwords = list()
+
+    with open(path, "r") as file:
+        info_reader = file.read()
+
+    info_reader = json.loads(info_reader)
+
+    for i in range(len(info_reader["encrypted_password"])):
+        key = info_reader["key"][i].encode()
+        encrypted = info_reader["encrypted_password"][i].encode()
+        password = encrypt.decoding(key, encrypted)
+        print(f"{i}_ {password}")
 
 
-lenght = input_handler("Password Lenght(8)", low = 1, Type = "int", default = 8)
-include_symbols = input_handler("Include symbols(Y/n)", options=["y", "n"], default = "y")
-include_numbers = input_handler("Include symbols(Y/n)", options=["y", "n"], default = "y")
-how_many_repeat = input_handler("How many passwords do you want(1)", low = 1, Type = "int", default = 1)
+option = input_handler("[C]reate new password or [s]how past password(C/s)", options=["c", "s"], default="c")
 
-include_symbols = False if include_symbols == "n" else True
-include_numbers = False if include_numbers == "n" else True
+if option == "c":
+    lenght = input_handler("Password Lenght(8)", low = 1, Type = "int", default = 8)
+    include_symbols = input_handler("Include symbols(Y/n)", options=["y", "n"], default = "y")
+    include_numbers = input_handler("Include symbols(Y/n)", options=["y", "n"], default = "y")
+    how_many_repeat = input_handler("How many passwords do you want(10)", low = 1, Type = "int", default = 10)
 
-items = mixer(include_symbols, include_numbers)
+    include_symbols = False if include_symbols == "n" else True
+    include_numbers = False if include_numbers == "n" else True
 
-print()
-password = []
+    items = mixer(include_symbols, include_numbers)
 
-for i in range(how_many_repeat):
-    password.append(password_generator(items, lenght = lenght))
-    print(f"{i + 1}- {password[i]}")
+    print()
+    password = []
 
-print("\nCopy any passwords you want because they will be encrypted after saving.")
+    for i in range(how_many_repeat):
+        password.append(password_generator(items, lenght = lenght))
+        print(f"{i + 1}- {password[i]}")
 
-want_save = input_handler("\nWrite the number of things you want to save with a space(everything = Enter, nothing = 0)", low=0, high=len(password), default=password)
-saving(0, password) if want_save == 0 else saving(want_save, password)
+    print("\nCopy any passwords you want because they will be encrypted after saving.")
+
+    want_save = input_handler("\nWrite the number of things you want to save with a space(everything = Enter, nothing = 0)", low=0, high=len(password), default=password)
+    save_password(0, password) if want_save == 0 else save_password(want_save, password)
+    print("Your voice save in: ", Path.cwd().joinpath("output.Jaraare"))
+
+else:
+    path = input_handler("Enter path your save password or contintue default", default=Path.cwd().joinpath("output.Jaraare"), automate=False)
+    show_password(path)
